0e29986c3ffcbf4d67fe75c11952d10be56d8f92
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-03-09 01:18:34 +0500

    tailwind-config
    tailwind-config and hello tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="flex spc justify-center align-middle w-full md:hidden">
+          <h1 >Another h1</h1>
+          <h1 className="hello-world">Intro To Sockets</h1></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
