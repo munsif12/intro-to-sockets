2bad6aacb51d34876d103f925f410268badd0a2c
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-03-26 04:14:26 +0500

    live-chart-completed

--- client/src/pages/LiveChart.js
+++ client/src/pages/LiveChart.js
@@ -4,58 +4,113 @@ import Chart from 'react-apexcharts'
 
 
 const ENDPOINT = process.env.ENDPOINT || "http://localhost:4001/";
-const options = {
-    chart: {
-        id: "basic-bar"
-    },
-    xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-    }
-}
 function LiveChart() {
 
     const socket = Socket(ENDPOINT);
     const [Status, setStatus] = useState('resume');
-    const [XY, setXY] = useState({
-        X: [],
-        Y: []
-    });
+    const [XY, setXY] = useState({ X: [49], Y: [63] });
+
+    //chart options and series
+    const RealTIme = {
+        series: [{
+            name: "Live Chart",
+            data: XY.X
+        }],
+        options: {
+            chart: {
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 1000
+                    }
+                },
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: true
+                },
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    shadeIntensity: 1,
+                    inverseColors: false,
+                    opacityFrom: 0.5,
+                    opacityTo: 0,
+                    stops: [0, 90, 100]
+                    // type: [0, 0, 0]
+                },
+            },
+            grid: {
+                show: false,
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            yaxis: {
+                max: 100,
+                title: {
+                    text: 'Random'
+                },
+                color: 'white'
+            },
+            xaxis: {
+                title: {
+                    text: 'Random'
+                },
+                style: {
+                    color: 'white'
+                }
+            },
+
+            title: {
+                text: 'Dynamicly Updating Chart Using Socket.io',
+                align: 'left',
+                style: {
+                    fontSize: '18px',
+                    color: 'Black'
+                }
+            },
+            markers: {
+                size: 0
+            },
+            tooltip: {
+                show: false
+            },
+            legend: {
+                show: true
+            },
+        }
+    };
+
+    const stopLiveData = () => {
+        socket.emit('stopLiveData');
+        setStatus('pause');
+    }
     useEffect(() => {
         //to trigger the event on the server
         socket.emit('getChartValuesXY', { id: 2 });
         // to get the data after the event is triggered
         socket.on('result', data => {
-            setXY(prevValue => {
-                return {
-                    X: [...prevValue.X, data.X],
-                    Y: [...prevValue.Y, data.Y]
-                }
-            })
+            setXY({ X: [...XY.X, data.X], Y: [...XY.Y, data.Y] })
         })
-    }, []);
 
-    const stopLiveData = () => {
-        if (Status === 'resume') {
-            socket.emit('stopLiveData');
-            setStatus('pause')
-        }
-        else {
-            socket.emit('getChartValuesXY');
-            setStatus('resume')
-        }
-    }
+    }, [XY, socket]);
+
     return (
-        <div className='text-slate-50'>
-            {/* <Chart
-                options={options}
-                series={XY.X}
-                type="line"
-                width="500"
-            /> */}
-            <p>X {'==>'} {XY.X}</p>
-            <br></br>
-            <p>Y {'==>'} {XY.Y}</p>
-            <button onClick={stopLiveData} className='btnLiveDataStop px-2 py-1 border-color-[white]'>{Status === 'resume' ? 'Stop Live Data' : 'Resume Live Data'}</button>
+        <div className='LiveChart bg-white text-slate-50 h-full  flex justify-center items-center flex-col' >
+            <Chart
+                options={RealTIme.options}
+                series={RealTIme.series}
+                type="area"
+                width='800'
+            />
+            <button onClick={stopLiveData} className='btnLiveDataStop text-black px-2 py-1 border-color-[black]'>{Status === 'resume' ? 'Stop Live Data' : 'Live Data Stopped'}</button>
         </div>
     )
 }
