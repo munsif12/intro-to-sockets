0a44d57e4bbde4322e300288a39e729afb0d2382
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-03-28 01:18:32 +0500

    main-ui-completed

--- client/src/pages/RealtimeChat.js
+++ client/src/pages/RealtimeChat.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 function RealtimeChat() {
+    function sendChat() {
+        alert('In Progress!');
+    }
     return (
         <div className='realtimeChat h-full flex justify-center items-center text-white'>
-            <div className="chat-container relative w-2/3 h-2/3 bg-gray-100">
+            <div className="chat-container relative xs:w-[80%] sm:w-[70%] md:w-[55%] lg:w-[55%] h-2/3 bg-gray-100 rounded-lg">
                 <div className="chat-header absolute top-[-25px] left-0">
                     <div className="chat-header-title ">
                         <h1 className='text-white'>Realtime Chat</h1>
@@ -19,22 +22,16 @@ function RealtimeChat() {
                                 <p>3:00</p>
                             </div>
                         </div>
-                        <div className="message">
-                            <div className="message-text">
-                                <p>Hello</p>
-                            </div>
-                            <div className="message-time">
-                                <p>3:00</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
-                <div className="chat-footer flex absolute bottom-0 left-0 w-full h-[12%]">
-                    <textarea placeholder="Type a message..." className="chat-textarea" />
-                    <div className="chat-footer-buttons ">
-                        <button className="chat-send-button">
-                            <p>Send Chatt</p>
-                        </button>
+                <div className="chatFooterParent absolute bottom-[-7.5%] left-0 w-full h-[15%] px-2 flex justify-center ">
+                    <div className="chat-footer flex justify-evenly w-[80%] bg-[#be2c6a] h-full rounded-lg ">
+                        <input type='text' className="chat-textarea outline-none w-2/3 px-3 text-black placeholder-white bg-transparent" placeholder="Type a message..." />
+                        <div className="chat-footer-buttons w-[20%] flex justify-center items-center">
+                            <button className="chat-send-button w-full h-[75%] bg-[#fa63a382] border-0 rounded-lg shadow-[rgb(174 174 174 / 10%) 0px 20px 25px -5px, rgb(253 253 253 / 1%) 0px 10px 10px -5px]">
+                                <p className='m-0 p-0' onClick={sendChat}>Send Chat</p>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
