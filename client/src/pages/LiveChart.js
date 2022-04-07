6c3fddba8f08e1943e423eb2a6b6166d21b7f132
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-04-07 08:35:34 +0500

    XY-chart+bug-fixed

--- client/src/pages/LiveChart.js
+++ client/src/pages/LiveChart.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import Socket from 'socket.io-client'
 import Chart from 'react-apexcharts'
 
-
-const ENDPOINT = process.env.ENDPOINT || "http://localhost:4001/";
-function LiveChart() {
-
-    const socket = Socket(ENDPOINT);
+function LiveChart({ socket }) {
+    //states
     const [Status, setStatus] = useState('resume');
-    const [XY, setXY] = useState({ X: [49], Y: [63] });
+    const [XY, setXY] = useState({ X: [49, 23, 68, 37], Y: [63, 88, 56, 24] });
+
+    useEffect(() => {
+        //to trigger the event on the server
+        socket.emit('getChartValuesXY');
+    }, [socket]);
+
+    useEffect(() => {
+        // to get the data after the event is triggered
+        socket.on('result', data => {
+            if (XY.X.length >= 10) {
+                setXY({ X: [...XY.X.filter((val, i) => i !== 0), data.X], Y: [...XY.Y.filter((val, i) => i !== 0), data.Y] })
+            }
+            else
+                setXY({
+                    X: [...XY.X, data.X], Y: [...XY.Y, data.Y]
+                })
+        }, [XY]);
+    });
 
     //chart options and series
     const RealTImeSeriesAndOptions = {
@@ -16,10 +30,10 @@ function LiveChart() {
             name: "Live Chart X",
             data: XY.X,
         },
-        /* {
+        {
             name: "Live Chart Y",
             data: XY.Y,
-        } */],
+        }],
         options: {
             chart: {
                 animations: {
@@ -78,37 +92,37 @@ function LiveChart() {
             markers: {
                 size: 0
             },
-            tooltip: {
-                show: true,
-                style: {
-                    fontSize: '18px',
-                    color: 'Black'
-                }
-            },
+
             legend: {
                 show: true
             },
+            tooltip: {
+                enabled: true,
+                enabledOnSeries: undefined,
+                shared: true,
+                followCursor: false,
+                intersect: false,
+                inverseOrder: false,
+                custom: undefined,
+                fillSeriesColor: false,
+                theme: "dark",
+                style: {
+                    fontSize: '12px',
+                    color: 'Black'
+                },
+                onDatasetHover: {
+                    highlightDataSeries: true,
+                },
+
+            }
         }
     };
 
+    //stop live data
     const stopLiveData = () => {
         socket.emit('stopLiveData');
         setStatus('pause');
     }
-    useEffect(() => {
-        //to trigger the event on the server
-        socket.emit('getChartValuesXY', { id: 2 });
-        // to get the data after the event is triggered
-        socket.on('result', data => {
-            if (XY.X.length >= 10) {
-                setXY({ X: [...XY.X.shift(), data.X], Y: [...XY.Y.shift(), data.Y] })
-            }
-            else
-                setXY({ X: [...XY.X, data.X], Y: [...XY.Y, data.Y] })
-        })
-
-    }, [XY, socket]);
-
     return (
         <div className='LiveChart bg-white text-slate-50 h-full  flex justify-center items-center flex-col' >
             <Chart
